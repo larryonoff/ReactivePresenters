4598efd58a34715c4ce5adcfe9ec5371f39f28e9
Ilya Laryionau <larryonoff@gmail.com>
2016-06-02 21:36:49 +0300

    add UIViewController presenters

---
+++ ReactiveCocoaPresenters/UIViewController.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewController.swift
+//  ReactivePresenters
+//
+//  Created by Ilya Laryionau on 02/06/16.
+//  Copyright © 2016 Ilya Laryionau. All rights reserved.
+//
+
+#if os(iOS) || os(tvOS)
+    
+import Foundation
+import UIKit
+import ReactiveCocoa
+import ReactivePresenters
+
+extension UIViewController {
+    public var rxp_title: Presenter<String?> {
+        return Presenter { [weak self] in
+            self?.title = $0
+            
+            return nil
+        }
+    }
+}
