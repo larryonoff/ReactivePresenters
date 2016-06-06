29616cf6c041a2c65f5af79117c4d950cb9f8690
Ilya Laryionau <larryonoff@gmail.com>
2016-06-06 16:15:01 +0300

    add TableViewCell

---
+++ ReactiveCocoaPresenters/UITableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  UITableViewCell.swift
+//  ReactivePresenters
+//
+//  Created by Ilya Laryionau on 06/06/16.
+//  Copyright © 2016 Ilya Laryionau. All rights reserved.
+//
+
+import UIKit
+import ReactiveCocoa
+import enum Result.NoError
+
+public class TableViewCell: UITableViewCell {
+
+    private let prepareForReusePipe = Signal<Void, NoError>.pipe()
+
+    public var rxp_prepareForReuse: Signal<Void, NoError> {
+        return prepareForReusePipe.0
+    }
+
+    //
+
+    override func prepareForReuse() {
+        prepareForReusePipe.1.sendNext()
+        
+        super.prepareForReuse()
+    }
+}
