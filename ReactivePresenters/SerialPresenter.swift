e9047bd94666e9662f994e6d6a0ae328e74bf1dd
Ilya Laryionau <larryonoff@gmail.com>
2016-07-08 13:02:09 +0300

    rename func serialized() into property serialized

--- ReactivePresenters/SerialPresenter.swift
+++ ReactivePresenters/SerialPresenter.swift
@@ -11,48 +11,48 @@ import ReactiveCocoa
 public final class SerialPresenter<Element>: PresenterProtocol {
 
     internal let binding: (Element) -> Disposable?
-    
+
     public init(_ binding: (Element) -> Disposable?) {
         self.binding = binding
     }
-    
+
     public init(_ binding: (Element) -> Presenter<Void>) {
         self.binding = { element in
             return binding(element).binding()
         }
     }
-    
+
     deinit {
         _disposable?.dispose()
     }
-    
+
     private var _disposable: SerialDisposable?
-    
+
     private var disposable: SerialDisposable {
         if let disposable = _disposable where !disposable.disposed {
             return disposable
         }
-        
+
         let newDisposable = SerialDisposable()
         _disposable = newDisposable
         return newDisposable
     }
-    
-    // PresenterProtocol
-    
+
+    // MARK: PresenterProtocol
+
     public func present(element: Element) -> Disposable? {
         let disposable = CompositeDisposable()
-        
+
         disposable += UIScheduler()
             .schedule { self.disposable.innerDisposable = self.binding(element) }
-        
+
         return disposable
     }
 }
 
 extension Presenter {
     ///
-    public func serialized() -> SerialPresenter<Element> {
+    public var serialized: SerialPresenter<Element> {
         return SerialPresenter(binding)
     }
 }
